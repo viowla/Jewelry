1f6bdbf3b89061ee94b2705b9c0128faff5b2d38
viowla <55015538+viowla@users.noreply.github.com>
2021-03-14 18:14:54 +0600

    Add sort method

--- src/main/java/service/MineralService.java
+++ src/main/java/service/MineralService.java
@@ -2,7 +2,8 @@ package service;
 
 import entity.Mineral;
 
-import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 public class MineralService {
@@ -10,10 +11,18 @@ public class MineralService {
 
     public double calculateCost(List<Mineral> necklace){
         for (Mineral mineral:necklace){
-            cost = cost+mineral.cost();
+            cost = cost+(mineral.cost() * mineral.getCarat());
         }
         return cost;
     }
 
+    public void sortMineral(List<Mineral> necklace){
+        Collections.sort(necklace, new Comparator<Mineral>() {
+            @Override
+            public int compare(Mineral o1, Mineral o2) {
+                return o1.getDescription().compareTo(o2.getDescription());
+            }
+        });
+    }
 
 }
