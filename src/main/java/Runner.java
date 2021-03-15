a88d8657a5f871aba102e9cdb9dacba87677ad7a
viowla <55015538+viowla@users.noreply.github.com>
2021-03-15 12:12:41 +0600

    Create main runner class

--- src/main/java/Runner.java
+++ src/main/java/Runner.java
@@ -1,5 +1,56 @@
+import entity.Mineral;
+import entity.gems.Diamond;
+import entity.gems.Gem;
+import entity.gems.Sapphire;
+import entity.semiprecious.Amethyst;
+import entity.semiprecious.SemipreciousStone;
+import entity.semiprecious.Topaz;
+import service.MineralService;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Runner {
     public static void main(String[] args) {
 
+        //Create minerals with definite weight in carat and necklace
+        List<Mineral> necklace = new ArrayList<>();
+        Mineral diamond = new Diamond(new Gem());
+        Mineral sapphire = new Sapphire(new Gem());
+        Mineral topaz = new Topaz(new SemipreciousStone());
+        Mineral amethyst = new Amethyst(new SemipreciousStone());
+
+        diamond.setCarat(2);
+        sapphire.setCarat(4);
+        topaz.setCarat(3);
+        amethyst.setCarat(5);
+
+        necklace.add(diamond);
+        necklace.add(sapphire);
+        necklace.add(topaz);
+        necklace.add(amethyst);
+
+        //Sorting minerals and calculating necklace cost
+        MineralService mineralService=new MineralService();
+        mineralService.sortMineral(necklace);
+        double cost = mineralService.calculateCost(necklace);
+
+        System.out.println(necklace);
+        System.out.println(cost);
+        
+        //Write results in file
+        try {
+            FileWriter myWriter = new FileWriter("filename.txt");
+            myWriter.write(String.valueOf(cost));
+            myWriter.write(System.getProperty( "line.separator" ));
+            myWriter.write(necklace.toString());
+            myWriter.close();
+            System.out.println("Successfully wrote to the file.");
+        } catch (IOException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+        }
     }
+
 }
